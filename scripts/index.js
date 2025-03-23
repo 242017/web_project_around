ee60f1e97aa7feb3a576c6c3e94c0d588a8fb1b6
Nicolas17 <nicolasibague2420@hotmail.com>
2025-03-23 09:28:57 -0500

    agrego archivo overlay.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const popupButton = document.querySelector(".popup__button");
 const button = document.querySelector(".profile__nameeditbutton");
 const popup = document.querySelector("#popup");
+const overlay = document.getElementById("overlay");
 const inputname = document.querySelector("#name");
 const inputlastname = document.querySelector("#subname");
 
@@ -12,6 +13,7 @@ const popupclose = document.querySelector(".popup__close");
 button.addEventListener("click", () => {
   console.log("Hola Mundo");
   popup.classList.toggle("popup_invisible");
+  overlay.classList.add("overlay_active");
 });
 
 popupButton.addEventListener("click", (e) => {
@@ -19,8 +21,10 @@ popupButton.addEventListener("click", (e) => {
   profilename.textContent = inputname.value;
   profilesubname.textContent = inputlastname.value;
   popup.classList.toggle("popup_invisible");
+  overlay.classList.remove("overlay_active");
 });
 
 popupclose.addEventListener("click", () => {
   popup.classList.add("popup_invisible");
+  overlay.classList.remove("overlay_active");
 });
